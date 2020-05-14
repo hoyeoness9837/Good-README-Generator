73a91da0e595c951d050f0177f92768933541110
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-05-14 01:24:52 -0700

    updated with hw

---
+++ Develop/README.md
@@ -0,0 +1,17 @@
+# App title: README.md generator
+## Table of concents
+*Develop file, index.js for exe file
+## How to install
+*Use git clone of the url to gitbash
+## App usage
+*to generate readme file easily
+## Licenses
+*undefined
+## Contributors
+*none
+## Tests to be run on app
+*none
+## Questions
+*none
+![Profile Picture](https://avatars3.githubusercontent.com/u/58324084?v=4)
+##Email: null
--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,58 @@
-const questions = [
+const { prompt } = require('inquirer');
+const axios = require('axios');
+const { writeFile } = require('fs');
+const { promisify } = require('util');
+const writeToFile = promisify(writeFile);
 
-];
-
-function writeToFile(fileName, data) {
-}
-
-function init() {
-
-}
-
-init();
+prompt([
+  { type: 'input', name: 'username', message: 'Type in your Github username' },
+  { type: 'input', name: 'title', message: 'Type in your title of this app' },
+  { type: 'input', name: 'description', message: 'Type in your description' },
+  {
+    type: 'input',
+    name: 'tableOfContent',
+    message: 'Type in your table of content',
+  },
+  { type: 'input', name: 'installation', message: 'Type in your installation' },
+  { type: 'input', name: 'usage', message: 'Type in your usage' },
+  {
+    type: 'input',
+    name: 'contributors',
+    message: 'Type in your contributors for this app',
+  },
+  { type: 'input', name: 'tests', message: 'Type in your tests for this app' },
+  {
+    type: 'input',
+    name: 'question',
+    message: 'Type in your question for this app',
+  },
+])
+  .then((data) => {
+    axios
+      .get(`http://api.github.com/users/${data.username}`)
+      .then((githubinfo) => {
+        writeToFile(
+          'README.md',
+          `# App title: ${data.title}
+## Table of concents
+*${data.tableOfContent}
+## How to install
+*${data.installation}
+## App usage
+*${data.usage}
+## Licenses
+*${data.licence}
+## Contributors
+*${data.contributors}
+## Tests to be run on app
+*${data.tests}
+## Questions
+*${data.question}
+![Profile Picture](${githubinfo.data.avatar_url})
+##Email: ${githubinfo.data.email}
+`
+        );
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
